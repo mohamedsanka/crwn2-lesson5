78fdb7a77f91b25c71a3d094af3df0f910c0ae4a
Mohamed Sanka <80225616+mohamedsanka@users.noreply.github.com>
2026-05-30 23:25:53 -0700

    firebaseAUTH2

--- src/routes/sing-in/sign-in.component.jsx
+++ src/routes/sing-in/sign-in.component.jsx
@@ -1,15 +1,37 @@
-import {signInWithGooglePopup, createUserDocumentFromAuth} from '../../utils/firebase/firebase.utils';
+import { getRedirectResult } from 'firebase/auth';
+import {
+    auth,
+    signInWithGooglePopup,
+    signInWithGoogleRedirect,
+    createUserDocumentFromAuth,} from '../../utils/firebase/firebase.utils';
+import { useEffect } from 'react';
 
 
 const SignIn = () =>{
+
+ useEffect(async () =>{
+    const response = await getRedirectResult(auth)
+    if(response){
+        const userDocRef = await createUserDocumentFromAuth(response.user)
+    }
+    
+ }, []) 
+
+ 
+
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopup();
         createUserDocumentFromAuth(user);
     }
+     const logGoogleRedirect = async () => {
+     const {user} = await signInWithGoogleRedirect();
+     createUserDocumentFromAuth(user);
+     }
     return (
     <div>
         <h1>I am the sign in page</h1>
         <button onClick={logGoogleUser}>Sign in with Google</button>
+         <button onClick={logGoogleUser}>Sign in with Redrect</button> 
     </div>
 );
 }
